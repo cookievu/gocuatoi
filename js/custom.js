5fe93ca0568f1d38974b73d245b11c515db26896
vu <vu@nvpcloud.com>
2019-08-31 23:11:13 +0700

    rebuilding site Sat Aug 31 23:11:13 +07 2019

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     // sidebar script
-    const h1 = document.body.getElementsByTagName("h1");
+    const h1 = document.body.querySelectorAll("article > h1");
     const sidebar = $('#content-sidebar');
     for (let i = 0; i < h1.length; i++) {
         sidebar.append(` <li><a href="#${h1[i].id}">${h1[i].textContent}</a></li>`)
