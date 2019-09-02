951f40004743c81177a70ad7e1d4e91ce654ff4b
vu <vu@nvpcloud.com>
2019-09-02 10:00:20 +0700

    rebuilding site Mon Sep  2 10:00:19 +07 2019

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,51 @@
 $(document).ready(function () {
     // sidebar script
-    const h1 = document.body.querySelectorAll("article > h1");
+    const list = parseList();
     const sidebar = $('#content-sidebar');
-    for (let i = 0; i < h1.length; i++) {
-        sidebar.append(` <li><a href="#${h1[i].id}">${h1[i].textContent}</a></li>`)
-    }
-
+    list.map(item => {
+        sidebar.append(`<li><a href="${item.href}">${item.title}</a></li>`);
+        if (item.child && item.child.length) {
+            const ul = document.createElement('ul');
+            item.child.map(e => ul.append(createListItem(e.href, e.title)));
+            sidebar.append(ul);
+        }
+    });
     initProgress();
 });
+
+function parseList() {
+    const list = [];
+    const h1 = document.body.querySelectorAll("article > h1, article > h2");
+    let node = null;
+    let latestTag = null;
+    for (let i = 0; i < h1.length; i++) {
+        if (h1[i].tagName === 'H1') {
+            node = createNote(h1[i].id, h1[i].textContent);
+        } else {
+            node.child.push(createNote(h1[i].id, h1[i].textContent));
+        }
+        if (h1[i].tagName === 'H1') {
+            list.push(node);
+        }
+        latestTag = h1[i].tagName;
+    }
+    console.log(list);
+    return list;
+}
+
+function createNote(href, title) {
+    return {href: '#' + href, title, child: []};
+}
+
+function createListItem(href, title) {
+    const childLi = document.createElement('li');
+    const aTag = document.createElement('a');
+    aTag.href = href;
+    const text = document.createTextNode(title);
+    aTag.append(text);
+    childLi.append(aTag);
+    return childLi;
+}
 
 function initProgress() {
     const progressBar = document.querySelector('#reading-progress');
